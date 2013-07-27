6610e0440ab3aec83a23fb1ee17193684a59e169
Gordon Guthrie <gordon@vixo.com>
2013-07-27 10:48:00 +0000

    start the web console and stuff

--- src/erlang_wtd_epmd_app.erl
+++ src/erlang_wtd_epmd_app.erl
@@ -21,9 +21,40 @@
 start() -> start([], []).
 
 start(_StartType, _StartArgs) ->
+
     ok = application:start(crypto),
     ok = application:start(ranch),
     ok = application:start(cowboy),
+
+    % Ret = application:get_env(erlang_wtd_epmd, web_page_port),
+    % io:format("application:get_env returns ~p~n", [Ret]),
+    % Ret2 = application:get_all_env(),
+    % io:format("application:get_all_env returns ~p~n", [Ret2]),
+    %{ok, Port} = Ret,
+
+    Port = 14151,
+
+    %Dispatch = cowboy_router:compile([{'_', [{'_', web_page, []}]}]),
+
+    Directory = {directory, "/home/vagrant/erlang-wtd-epmd/var/docroot/"},
+    MimeTypes = {mimetypes, {fun mimetypes:path_to_mimes/2, default}},
+
+    io:format("Directory is ~p MimeTypes is ~p~n", [Directory, MimeTypes]),
+
+    Dispatch = [{'_', [
+                       {"/[...]", cowboy_static, [
+                                                  Directory,
+                                                  MimeTypes
+                                                 ]}
+                      ]}
+               ],
+
+    CDispatch = cowboy_router:compile(Dispatch),
+
+    {ok, _PID} = cowboy:start_http(web_page_listener, 100,
+                           [{port, Port}],
+                           [{env, [{dispatch, CDispatch}]}]),
+
     erlang_wtd_epmd_sup:start_link().
 
 stop(_State) ->
--- src/web_page.erl
+++ src/web_page.erl
@@ -7,4 +7,20 @@
 
 -module(web_page).
 
+-behaviour(cowboy_http_handler).
 
+-export([
+         init/3,
+         handle/2,
+         terminate/3
+        ]).
+
+init({tcp, http}, Req, _Opts) ->
+    {ok, Req, undefined_state}.
+
+handle(Req, State) ->
+    {ok, Req2} = cowboy_req:reply(200, [], <<"hey!">>, Req),
+    {ok, Req2, State}.
+
+terminate(_Reason, _Req, _State) ->
+    ok.
